b11d7e254aac7a3087093f995d3c315dbe2701f9
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-14 23:36:58 +0300

    initialize the attributes

--- class/item.rb
+++ class/item.rb
@@ -1,8 +1,12 @@
 class Item
+ 
+ attr_accessor :id, :publish_date
+ attr_reader :archive
 
- def initialize(genre, author, source, label, published_date)
+ def initialize(id, publish_date, archive)
   @id = random.rand(1..1000)
-
+  @publish_date = publish_date
+  @archive = false
  end
 
 end
