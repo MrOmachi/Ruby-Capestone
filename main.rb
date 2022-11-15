bf1021734be9ef247ef6e2c8738dc3e9a517cc9f
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-11-15 13:40:02 -0600

    Apply the display menu

--- main.rb
+++ main.rb
@@ -7,60 +7,34 @@ def display_options
               10 => 'Add a game', 11 => 'Exit' }
   options.each { |key, value| puts "#{key} - #{value}\n" }
   choice = gets.chomp.to_i
-
-  # list_all_books if choice == 1
-  # list_all_music_albums if choice == 2
-  # list_of_games if choice == 3
-  # list_all_genres if choice == 4
-  # list_all_labels if choice == 5
-  # list_all_authors if choice == 6
-  # list_all_sources if choice == 7
-  # add_a_book if choice == 8
-  # add_a_music_album if choice == 9
-  # add_a_game if choice == 10
-  # exit if choice == 11
+  if choice >= 1 && choice <= 11
+    choose_option(choice)
+  else
+    puts 'Select a valid option'
+  end
 end
 
-# def choose_option(choice)
-#   case choice
-#   when 1
-#     list_all_books
-#   when 2
-#     list_all_music_albums
-#   when 3
-#     list_of_games
-#   when 4
-#     list_all_genres
-#   when 5
-#     list_all_labels
-#   when 6
-#     list_all_authors
-#   when 7
-#     list_all_sources
-#   when 8
-#     add_a_book
-#   when 9
-#     add_a_music_album
-#   when 10
-#     add_a_game
-#   else
-#     exit
-#   end
-# end
+def choose_option(choice)
+  list_all_books if choice == 1
+  list_all_music_albums if choice == 2
+  list_of_games if choice == 3
+  choose_option_one(choice)
+end
 
-# def choose_option(choice)
-#   list_all_books if choice == 1
-#   list_all_music_albums if choice == 2
-#   list_of_games if choice == 3
-#   list_all_genres if choice == 4
-#   list_all_labels if choice == 5
-#   list_all_authors if choice == 6
-#   list_all_sources if choice == 7
-#   add_a_book if choice == 8
-#   add_a_music_album if choice == 9
-#   add_a_game if choice == 10
-#   break if choice == 11
-# end
+def choose_option_one(choice)
+  list_all_genres if choice == 4
+  list_all_labels if choice == 5
+  list_all_authors if choice == 6
+  choose_option_two(choice)
+end
+
+def choose_option_two(choice)
+  list_all_sources if choice == 7
+  add_a_book if choice == 8
+  add_a_music_album if choice == 9
+  add_a_game if choice == 10
+  exit if choice == 11
+end
 
 def main
   app = App.new
