940c9efad1c47e37196e271f0f18e11280344ec9
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-11-15 10:13:43 -0600

    Add Genre file & music_album file

---
+++ class/genre.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Genre class
+class Genre
+  attr_accessor :id, :name, :items
+
+  def initialize(name, items = [])
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = items
+  end
+
+  def add_item
+    @items << item
+    item.genre = self
+  end
+end
