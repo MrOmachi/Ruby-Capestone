671084c3192d2407a7573918563b02ae297fa13b
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-17 17:05:52 +0300

    add test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,32 @@
 # test book
+require './class/book'
+
+describe Book do
+  before :each do
+    @book = Book.new('Elsiever', 'Good', '12-01-2016')
+  end
+
+  it 'Return a book' do
+    expect(@book).to be_a_instance_of Book
+  end
+
+  it 'Book has a publisher' do
+    expect(@book.publisher).to eq 'Elsiever'
+  end
+
+  it 'Book should have a cover state' do
+    expect(@book.cover_state).to eq 'Good'
+  end
+
+  it 'Book should have a publish date' do
+    expect(@book.publish_date.strftime('%d-%m-%Y')).to eq('12-01-2016')
+  end
+
+  it 'Book should not be archived' do
+    expect(@book.archived).to eq false
+  end
+
+  it 'Book should have an id' do
+    expect(@book.id).to be_a_kind_of Integer
+  end
+end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,1 +1,20 @@
 # test label
+require './class/label'
+
+describe Label do
+  before :each do
+    @label = Label.new('Programming', 'red')
+  end
+
+  it 'Return a label' do
+    expect(@label).to be_a_instance_of Label
+  end
+
+  it 'Label has a title' do
+    expect(@label.title).to eq 'Programming'
+  end
+
+  it 'Label has a color' do
+    expect(@label.color).to eq 'red'
+  end
+end
